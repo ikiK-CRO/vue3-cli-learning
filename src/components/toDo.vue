5457a8adfb0af286bef0786264715fecb257a785
ikiK <kiki.ikik@gmail.com>
2021-04-28 15:26:43 +0200

    -a

--- src/components/toDo.vue
+++ src/components/toDo.vue
@@ -15,14 +15,15 @@ export default {
       todo: [
         { id: 1, text: "Setup initial UI ✓"},
         { id: 2, text: "Setup some Vue 3 compatible component library" },
-        { id: 3, text: "Setup to do list" },
-        { id: 4, text: "Setup menu" },
-        { id: 5, text: "Setup DB Cloud Firestore connectivity" },
-        { id: 6, text: "Research Vue routing" },
-        { id: 7, text: "Create info section about every to do item" },
-        { id: 8, text: "Create log in system using Firebase Authentication" },
-        { id: 9, text: "Create simple websocket chat for logged in guests"},
-        { id: 10, text: "and we will see..."}
+        { id: 3, text: "Prevent Heroku app from spleep" },
+        { id: 4, text: "Setup to do list" },
+        { id: 5, text: "Setup menu" },
+        { id: 6, text: "Setup DB Cloud Firestore connectivity" },
+        { id: 7, text: "Research Vue routing" },
+        { id: 8, text: "Create info section about every to do item" },
+        { id: 9, text: "Create log in system using Firebase Authentication" },
+        { id: 10, text: "Create simple websocket chat for logged in guests"},
+        { id: 11, text: "..."}
       ]
     }
   },
